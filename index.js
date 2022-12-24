fa23a98a02bb48e2e04fc8529a832cf7621744e7
SafaElmali <tsafaelmali@gmail.com>
2022-12-24 20:56:57 +0300

    Add Vue options

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const questions = [
     type: "list",
     name: "framework",
     message: "Which framework would you like to use for your project?",
-    choices: ["Express", "NestJS", "React"],
+    choices: ["Express", "React", "Vue"],
   },
   {
     type: "checkbox",
@@ -29,23 +29,15 @@ const questions = [
     type: "checkbox",
     name: "packages",
     message: "Which popular packages would you like to install?",
-    when: (answers) => answers.framework.toLowerCase() === "nestjs",
-    choices: [
-      "@nestjs/common",
-      "@nestjs/core",
-      "@nestjs/platform-express",
-      "typeorm",
-      "@nestjs/jwt",
-      "@nestjs/passport",
-      "@nestjs/swagger",
-    ],
+    when: (answers) => answers.framework.toLowerCase() === "react",
+    choices: ["react-router-dom", "redux", "react-redux", "react-bootstrap"],
   },
   {
     type: "checkbox",
     name: "packages",
-    message: "Which popular packages would you like to install?",
-    when: (answers) => answers.framework.toLowerCase() === "react",
-    choices: ["react-router-dom", "redux", "react-redux", "react-bootstrap"],
+    message: "Which popular packages do you want to install?",
+    when: (answers) => answers.framework.toLowerCase() === "vue",
+    choices: ["vue-router", "vuex", "vue-bootstrap"],
   },
 ];
 
@@ -90,10 +82,6 @@ prompt(questions).then((answers) => {
   if (answers.framework.toLowerCase() === "express") {
     console.log(
       "Express is a minimalist web framework for Node.js that is easy to learn and use."
-    );
-  } else if (answers.framework.toLowerCase() === "nestjs") {
-    console.log(
-      "NestJS is a powerful and flexible web framework for Node.js that is great for building complex web applications."
     );
   } else if (answers.framework.toLowerCase() === "react") {
     console.log(
